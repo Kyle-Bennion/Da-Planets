35b676acba703063ffcd7e883fdc84ae6ecd1628
Kyle Bennion <bilekennion@hotmail.com>
2020-09-09 14:18:28 -0600

    Basic Set up done with Galaxy Controller Complete

---
+++ server/controllers/GalaxiesController.js
@@ -0,0 +1,61 @@
+import express from "express";
+import BaseController from "../utils/BaseController";
+import { galaxyService } from "../services/GalaxyService";
+
+export class GalaxiesController extends BaseController {
+  constructor() {
+    super("api/galaxies")
+    this.router
+      .get("", this.getAll)
+      .get("/:id", this.getById)
+      .post("", this.create)
+      .put("/:id", this.edit)
+      .delete("/:id", this.delete)
+  }
+  async getAll(req, res, next) {
+    try {
+      let data = await galaxyService.find(req.query)
+      res.send(data)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async getById(req, res, next) {
+    try {
+      let data = await galaxyService.findById(req.body)
+      res.send(data)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async create(req, res, next) {
+    try {
+      let data = await galaxyService.create(req.body)
+      res.status(201).send(data)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async edit(req, res, next) {
+    try {
+      req.body.id = req.params.id
+      let data = await galaxyService.edit(req.body)
+      res.send(data)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async delete(req, res, next) {
+    try {
+      await.galaxyService.delete(req.params.id)
+      res.send("Ahh HAHAHA Deleted")
+    } catch (error) {
+      next(error)
+    }
+  }
+
+}
